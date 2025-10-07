d895eda7800f1ef52d21a1da109b5d7778f9ebbf
Chase <sp24lights@gmail.com>
2025-10-07 12:21:16 -0400

    scaffolding

---
+++ src/main/java/com/example/animal_crud_api/AnimalCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.animal_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalCrudApiApplication.class, args);
+	}
+
+}
