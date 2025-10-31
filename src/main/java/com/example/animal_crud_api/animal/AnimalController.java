1bad8e080fd6def0ef0bccccfd5d82526ef5b02b
Chase <sp24lights@gmail.com>
2025-10-31 14:10:48 -0400

    backend debugging

--- src/main/java/com/example/animal_crud_api/animal/AnimalController.java
+++ src/main/java/com/example/animal_crud_api/animal/AnimalController.java
@@ -3,10 +3,12 @@ package com.example.animal_crud_api.animal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.ui.Model;
 
@@ -22,15 +24,14 @@ public Object getAllAnimals(Model model) {
     //return animalService.getAllAnimals();
     model.addAttribute("animalsList", animalService.getAllAnimals());
     model.addAttribute("title", "All Animals");
-    return "index"; //ftlh view name 
+    return "animal-list"; //ftlh view name 
 }
 
 @GetMapping("/animals/{animalId}")
 public String getAnimalById(@PathVariable Long animalId, Model model) {
-    //return animalService.getAnimalById(animalId);
-    model.addAttribute("animal", animalService.getAnimalById(animalId));
-    model.addAttribute("title", "animal #: " + animalId);
-    return "details";
+    Animal animal = animalService.getAnimalById(animalId);
+    model.addAttribute("animal", animal);
+    return "animal-details";
 }
 
 @GetMapping("/animals/classification/{classification}")
@@ -59,11 +60,12 @@ public Object showCreateForm(Model model) {
     Animal animal = new Animal();
     model.addAttribute("animal", animal);
     model.addAttribute("title", "Add new animal");
-    return "new-animal-form";
+    return "animal-create";
 }
 @PostMapping("/animals")
-public Object addAnimal(Animal animal, @RequestParam MultipartFile animalPicture) {
-    Animal newAnimal = animalService.addAnimal(animal, animalPicture);
+public Object addAnimal(
+        @ModelAttribute Animal animal) {
+    Animal newAnimal = animalService.addAnimal(animal);
     return "redirect:/animals/" + newAnimal.getAnimalId();
 }
 @GetMapping("/animals/updateForm/{animalId}")
@@ -74,8 +76,8 @@ public Object showUpdateForm(@PathVariable Long animalId, Model model) {
     return "animal-update";
 }
 @PostMapping("/animals/update/{animalId}")
-public Object updateAnimal(@PathVariable Long animalId, @RequestBody MultipartFile animalPicture, Animal animal) {
-    animalService.updateAnimal(animalId, animal, animalPicture);
+public Object updateAnimal(@PathVariable Long animalId, Animal animal) {
+    animalService.updateAnimal(animalId, animal);
     return "redirect:/animals/" + animalId;
 }
 @GetMapping("/animals/delete/{animalId}")
--- src/main/java/com/example/animal_crud_api/animal/AnimalService.java
+++ src/main/java/com/example/animal_crud_api/animal/AnimalService.java
@@ -1,6 +1,5 @@
 package com.example.animal_crud_api.animal;
 
-
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -9,7 +8,6 @@ import java.nio.file.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class AnimalService {
@@ -17,7 +15,6 @@ public class AnimalService {
 @Autowired
 private AnimalRepository animalRepository;
 
-private static final String UPLOAD_DIR = "src/main/resources/static/img/";
 
 //getMapping methods
 public Object getAllAnimals() {
@@ -36,47 +33,11 @@ public Object getAnimalsByName (String name) {
     return animalRepository.findByName(name);
 }
 
-public Animal addAnimal(Animal animal, MultipartFile animalPicture) {
-    Animal newAnimal = animalRepository.save(animal);
-    String originalFileName = animalPicture.getOriginalFilename();
-
-    try {
-      if (originalFileName != null && originalFileName.contains(".")) {
-        String fileExtension = originalFileName.substring(originalFileName.lastIndexOf(".") + 1);
-        String fileName = String.valueOf(newAnimal.getAnimalId()) + "." + fileExtension;
-        Path filePath = Paths.get(UPLOAD_DIR + fileName);
-
-        InputStream inputStream = animalPicture.getInputStream();
-
-        Files.createDirectories(Paths.get(UPLOAD_DIR));// Ensure directory exists
-        Files.copy(inputStream, filePath,
-            StandardCopyOption.REPLACE_EXISTING);// Save picture file
-        newAnimal.setAnimalPicturePath(fileName);
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-    return animalRepository.save(newAnimal);
+public Animal addAnimal(Animal animal) {
+    return animalRepository.save(animal);
 }
 
-public Animal updateAnimal(Long animalId, Animal animal, MultipartFile animalPicture) {
-    String originalFileName = animalPicture.getOriginalFilename();
-    try {
-      if (originalFileName != null && originalFileName.contains(".")) {
-        String fileExtension = originalFileName.substring(originalFileName.lastIndexOf(".") + 1);
-        String fileName = String.valueOf(animalId) + "." + fileExtension;
-        Path filePath = Paths.get(UPLOAD_DIR + fileName);
-
-        InputStream inputStream = animalPicture.getInputStream();
-        Files.deleteIfExists(filePath);
-        Files.copy(inputStream, filePath,
-            StandardCopyOption.REPLACE_EXISTING);// Save picture file
-        animal.setAnimalPicturePath(fileName);
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+public Animal updateAnimal(Long animalId, Animal animal) {
     return animalRepository.save(animal);
   }
 
